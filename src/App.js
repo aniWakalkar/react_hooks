36fceeb49723cce2221b9c9b30da4318b7e64a66
master <aniketwakalkar23@gmail.com>
2025-10-19 19:25:36 +0530

    react_hook project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-export default function App() {
+import React, { useState } from 'react';
+import { docs } from './data/docs';
+import Sidebar from './components/Sidebar';
+import DocViewer from './components/DocViewer';
+
+function App() {
+  const [selected, setSelected] = useState("useState");
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="min-h-screen bg-gray-100">
+      {/* Fixed Sidebar */}
+      <div className="fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-10">
+        <Sidebar topics={docs} onSelect={setSelected} />
+      </div>
+
+      {/* Main Content */}
+      <div className="ml-64 p-6">
+        <DocViewer doc={docs[selected]} />
+      </div>
+    </div>
+  );
 }
+
+export default App;
